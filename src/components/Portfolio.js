2bdeda534168a1908bd49676a0e693bde4846f5f
Lucas Sobrinho <lucasmsobrinho@gmail.com>
2022-01-06 07:03:06 -0300

    Portfolio cards bringup with responsive behavior

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,14 +1,75 @@
 import React from 'react';
 import './Portfolio.css';
 
-const Portfolio = () => { return(
-    <section id="portfolio">
-        <div className="description">
-            <h2>Portfolio</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae turpis massa sed elementum tempus egestas sed sed. Et tortor consequat id porta.</p>            
-        </div>
-    </section>
-)}
+const projects = [
+    { 
+        name: "placeholder project",
+        demoUrl: "https://www.google.com",
+        description: "Just a couple words. C'mon, it's better than Lorem Ipsum right?",
+        image: '/logo192.png'
+    }, { 
+        name: "placeholder project 2",
+        demoUrl: "https://www.google.com",
+        description: "The most useful infinite improbability generator.",
+        image: '/logo192.png'
+    }, { 
+        name: "placeholder project 3",
+        demoUrl: "https://www.google.com",
+        srcUrl: "https://www.github.com",
+        description: "The most useful infinite improbability generator.",
+        image: '/logo192.png'
+    }, { 
+        name: "placeholder project 4",
+        demoUrl: "https://www.google.com",
+        srcUrl: "https://www.github.com",
+        description: "The most useful infinite improbability generator.",
+        image: '/logo192.png'
+    },
+]
 
+const Portfolio = () => {
+    return(
+        <section id="portfolio">
+            <ProjectsContainer items={projects}/>
+        </section>
+    )
+}
+
+const ProjectsContainer = (props) => {
+    const portfolio = props.items.map( project => {
+        return <ProjectCard project={project}/>
+    })
+    return(
+        <div className="projects-container">
+            {portfolio}
+        </div>
+    )
+}
+
+const ProjectCard = (props) => {
+    return (
+        <div className="project-card">
+            <div className="project-image-container">
+                <a href={props.project.demoUrl}>
+                    <img src={props.project.image}/>
+                </a>
+            </div>
+            <div className="card-title">
+                {props.project.name}
+            </div>
+            <div className="card-description">
+                {props.project.description}
+            </div>
+            <div className="demo">
+                <a href={props.project.demoUrl}>demo</a>
+            </div>
+            {   props.project.srcUrl ? (
+                <div className="source-code">
+                    <a href={props.project.srcUrl}>source code</a>
+                </div>) : <div/>
+            }
+        </div>
+    )
+}
 
 export default Portfolio
